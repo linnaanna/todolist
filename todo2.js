43ed9f0999ecf9c6d9e63ded2de5d28f1a01deca
linnaanna <linna.anna@gmail.com>
2022-10-21 09:03:16 +0300

    Refactoring

--- todo2.js
+++ todo2.js
@@ -4,137 +4,128 @@ const todoForm = document.querySelector('.todo-form');
 const todoInput = document.querySelector('.todo-input');
 
 const todoItemsList = document.querySelector('.todo-items');
-// create array to store todos
+
+//create array for todos
 let todos = [];
-// add eventListener for submit 
-todoForm.addEventListener('submit', function(event) {
-// prevent reloading when submitting
-  event.preventDefault();
-// call addTodo function
-  addTodo(todoInput.value); 
-});
+
+//add eventListener for submit event to call add Todo function
+todoForm.addEventListener('submit', function (event) {
+event.preventDefault();//prevent reloading when submitting
+addTodo(todoInput.value);});
 
 
-// function to check for correcteness and add todo
+//check correcteness and add todo
 function addTodo(item) {
-    if (item === '') {
-        alert("Write something first!");
-        var element = document.getElementById("input");
-        setTimeout(function () {element.classList.add("wrong")}, 100);
-        setTimeout(function () {element.classList.remove("wrong")}, 1000);
-        
-      } else if (item.length < 4) {
-        alert("Input must be longer!");
-        var element = document.getElementById("input");
-        setTimeout(function () {element.classList.add("wrong")}, 100);
-        setTimeout(function () {element.classList.remove("wrong")}, 1000);
-    
-      } else {
-  
-  if (item !== '') {
-// make todo object with id, name, and completed status
-    const todo = {
-      id: Date.now(),
-      name: item,
-      completed: false
-    };
-// add to array and store
-    todos.push(todo);
-    addToLocalStorage(todos);
-// clear input box value
-    todoInput.value = '';
+  if (item === '') {
+    alert("Write something first!");
+    var element = document.getElementById("input");
+    setTimeout(function () { element.classList.add("wrong") }, 100);
+    setTimeout(function () { element.classList.remove("wrong") }, 1000);
+
+  } else if (item.length < 4) {
+    alert("Input must be longer!");
+    var element = document.getElementById("input");
+    setTimeout(function () { element.classList.add("wrong") }, 100);
+    setTimeout(function () { element.classList.remove("wrong") }, 1000);
+
+  } else {
+
+    if (item !== '') {
+      
+      const todo = { //todo object
+        id: Date.now(),
+        name: item,
+        completed: false
+      };
+      
+      todos.push(todo); //add to array and store
+      addToLocalStorage(todos);
+      todoInput.value = ''; //clear input box
+    }
   }
 }
-}
 
 
-//populate todos on screen
+//populate todos on screen, clear ul from class=todo-items
 function renderTodos(todos) {
-// clear everything in <ul> with class=todo-items
   todoItemsList.innerHTML = '';
-// run through items in todos
-  todos.forEach(function(item) {
-    // check if item is completed
-    const checked = item.completed ? 'checked': null;
-
-
-// make <li> element and fill it
+  todos.forEach(function (item) { //go through items in todos
     
+    const checked = item.completed ? 'checked' : null; //check if item is completed
+
+    //make <li> element and fill it, check for completed items 
+    //and add check
     const li = document.createElement('li');
-   
+
     li.setAttribute('class', 'item');
-    
+
     li.setAttribute('data-key', item.id);
-    
+
     if (item.completed === true) {
       li.classList.add('checked');
     }
-li.innerHTML = 
-`
-       <input type="checkbox" class="checkbox" ${checked}>
+    li.innerHTML =
+      `<input type="checkbox" class="checkbox" ${checked}>
       ${item.name}
-      <button class="close"></button>
-    `;
+      <button class="close"></button>`;
 
-
-//  add <li> to <ul>
+    //finally, add <li> to <ul>
     todoItemsList.append(li);
   });
 }
 
 
-//add todos to storage
+//add todos to storage and render on screen
 function addToLocalStorage(todos) {
-
   localStorage.setItem('todos', JSON.stringify(todos));
-// populate them on screen
   renderTodos(todos);
 }
 
 
-// get everything from local storage
+//get everything from local storage
 function getFromLocalStorage() {
   const reference = localStorage.getItem('todos');
-  
+
   if (reference) {
-    
     todos = JSON.parse(reference);
     renderTodos(todos);
   }
 }
-// set value to completed or not completed
+
+//set value to completed or not completed
 function toggle(id) {
-  todos.forEach(function(item) {
-    
+  todos.forEach(function (item) {
+
     if (item.id == id) {
-    
-      item.completed = !item.completed;
+        item.completed = !item.completed;
     }
   });
-addToLocalStorage(todos);
-}
-// deletes todo from todos array, updates localstorage and populates updated list on screen
-function deleteTodo(id) {
-// filters out <li> with id and updates todos array
-  todos = todos.filter(function(item) {
-   
-    return item.id != id;
-  });
-// update localStorage
   addToLocalStorage(todos);
 }
-// get everything from localStorage
+
+
+//delete todo from todos array, update localstorage
+//and render updated list on screen
+function deleteTodo(id) {
+  // filters out <li> with id and updates todos array
+  todos = todos.filter(function (item) {
+          return item.id != id;
+          });
+
+  addToLocalStorage(todos);
+}
+
+//get everything from localStorage
 getFromLocalStorage();
+
 //listen for click event in close-button and checkbox
-todoItemsList.addEventListener('click', function(event) {
-// check if the event is on checkbox
+todoItemsList.addEventListener('click', function (event) {
+  //check if the event is on checkbox and change state
   if (event.target.type === 'checkbox') {
-// toggle the state
-    toggle(event.target.parentElement.getAttribute('data-key'));
-  }
-// check if item has been closed
+      toggle(event.target.parentElement.getAttribute('data-key'));
+    }
+  //check if item has been closed and get id from closed
   if (event.target.classList.contains('close')) {
-// get id from data-key value where item has been closed
     deleteTodo(event.target.parentElement.getAttribute('data-key'));
   }
 });
